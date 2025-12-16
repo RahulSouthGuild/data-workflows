b8f372cc30646342406ea8a90adacd1653164312
Rahul South Guild <rahul@southguild.tech>
2025-12-16 16:58:20 +0530

    fix: Update blob paths in main function for accurate data retrieval

--- core/extractors/azure_extractor.py
+++ core/extractors/azure_extractor.py
@@ -235,8 +235,8 @@ async def main():
         "Historical_S_Historical/FactInvoiceSecondary/LatestData/",
         "Historical_S_Historical/FactInvoiceSecondary_107_112/LatestData/",
         # "Historical_S_Historical/FactInvoiceSecondary_107_112/PreviousData/",
-        # "Historical_S_Historical/FactInvoiceDetails_107_112/LatestData/",
-        # "Historical_S_Historical/FactInvoiceDetails/LatestData/",
+        "Historical_S_Historical/FactInvoiceDetails_107_112/LatestData/",
+        "Historical_S_Historical/FactInvoiceDetails/LatestData/",
         # "Historical_S_Historical/FactInvoiceDetails_107_112/PreviousData/",
     ]
 
